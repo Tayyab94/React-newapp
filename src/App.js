8b15d9f33c584dcd7aa0e4afd56c9ebfc8a7cd71
Tayyab94 <Tayyab.bhatti30@gmail.com>
2022-06-27 08:16:23 -0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={<News pageSize={6} key="general" category="general" />}
+            />
+            <Route
+              exact
+              path="/business"
+              element={<News pageSize={6} key="business" category="business" />}
+            ></Route>
+            <Route
+              exact
+              path="/entertainment"
+              element={
+                <News
+                  pageSize={6}
+                  category="entertainment"
+                  key="entertainment"
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/sports"
+              element={<News pageSize={6} category="sports" key="sports" />}
+            ></Route>
+            <Route
+              exact
+              path="/health"
+              element={<News pageSize={6} category="health" key="health" />}
+            ></Route>
+            <Route
+              exact
+              path="/science"
+              element={<News pageSize={6} category="science" key="science" />}
+            ></Route>
+            <Route
+              exact
+              path="/technology"
+              element={
+                <News pageSize={6} category="technology" key="technology" />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/"
+              element={<News pageSize={6} category="sports" key="sports" />}
+            ></Route>
+          </Routes>
+        </BrowserRouter>
+        {/* <News pageSize={6} category="sports" /> */}
+      </>
+    );
+  }
 }
 
 export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/components/NewItem.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+export class NewItem extends Component {
+  render() {
+    let { title, description, imageUrl, newsUrl } = this.props;
+    return (
+      <div>
+        <div className="card">
+          <img src={imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href={newsUrl} className="btn btn-primary">
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewItem;
+<h4>THis is News Item Component</h4>;
